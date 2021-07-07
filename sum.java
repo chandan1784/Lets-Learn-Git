37607697e8aa289484fd1a94c1aa0ecad35186fb
Chandan Kumar <chandankumar01784@gmail.com>
2021-07-08 00:34:33 +0530

    changed data in sysout

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public class sum{
     public static void main(String[] args) {
 int a = 65;
 int b = 35;
-System.out.println("The sum of a and b is = " + Integer.sum(a, b));
+System.out.println("The sum of a & b is = " + Integer.sum(a, b));
 } 
 }
